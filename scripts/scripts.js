220b59e898d0dfb879f15b9f3db5f0947277f85b
Александр Сергеев < >
2017-06-15 17:23:55 +0300

    Changing 4

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$.validator.addMethod("defaultInvalid", function(value, element) 
-	 {
-	  return !(element.value == element.defaultValue);
-	 });
-/**
-	$('.clients .slider_wrap .slider').owlCarousel({
+    $.validator.addMethod("defaultInvalid", function(value, element)
+    {
+        return !(element.value == element.defaultValue);
+    });
+    /**
+     $('.clients .slider_wrap .slider').owlCarousel({
 		autoPlay: true, 
 		nav : true, 
 		slideSpeed : 1300,
@@ -18,148 +18,148 @@ $(document).ready(function() {
 		autoHeight:true,
 		items: 1
 	});
-**/
-	$('header ul li a').click(function(e) {
+     **/
+    $('header ul li a').click(function(e) {
 
-		e.preventDefault();
-		var target = $(this).attr('href');
+        e.preventDefault();
+        var target = $(this).attr('href');
 
-		$('body,html').animate({ scrollTop: $(target).offset().top },1000);
+        $('body,html').animate({ scrollTop: $(target).offset().top },1000);
 
-	});
-
-
-	$('.top_screen .content a').click(function(e) {
-
-		e.preventDefault();
-
-		$('body,html').animate({ scrollTop: $('#footer').offset().top },1000);
-
-	});
+    });
 
 
-	$('.modal_open').click(function(e) {
+    $('.top_screen .content a').click(function(e) {
 
-		e.preventDefault();
+        e.preventDefault();
 
-		var target = $(this).attr('data-target');
-		console.log(target);
+        $('body,html').animate({ scrollTop: $('#footer').offset().top },1000);
 
-		$.fancybox.open('#modal' , {
-			beforeShow: function() {
-
-				$('#modal #formsended').val(target);
-			}
-		} );
-	});
+    });
 
 
+    $('.modal_open').click(function(e) {
+
+        e.preventDefault();
+
+        var target = $(this).attr('data-target');
+        console.log(target);
+
+        $.fancybox.open('#modal' , {
+            beforeShow: function() {
+
+                $('#modal #formsended').val(target);
+            }
+        } );
+    });
 
 
-$("#form").validate({
 
-		rules: {
-			name: {
-				required:true,
-				defaultInvalid: true,
-				minlength:3
-			},
-			phone: {
-				required:true,
-				defaultInvalid: true,
-				minlength:8
-			},
-			mail:{
-				required: true,
-				defaultInvalid: true,
-				minlength:1
-			},
-		},
-		 messages: {
-		 	name: {
-                required: "Введите ваше имя.",
+
+    $("#form").validate({
+
+        rules: {
+            name: {
+                required:true,
+                defaultInvalid: true,
+                minlength:3
             },
             phone: {
-                required: "Введите правильный номер.",
-                minlength: "Введите правильный номер.",
+                required:true,
+                defaultInvalid: true,
+                minlength:8
             },
-            mail: {
-                required: "Введите ваш Email.",
-                minlength: "Введите ваш Email.",
-                mail: "Неверно заполнено поле.",
+            mail:{
+                required: true,
+                defaultInvalid: true,
+                minlength:1
             },
-
-          },
-		submitHandler: function(form) {
-			$.ajax({
-				type: 'POST',
-				url: 'mailer.php',
-				data:  msg,
-				success: function(data) {
-					$.fancybox.open('#success');           
-				},
-				error:  function(xhr, str){
-					$.fancybox.open('#notsended');
-				}
-			});
-			return false;
-		}
-	});
-
-$("#form2").validate({
-
-		rules: {
-			name: {
-				required:true,
-				defaultInvalid: true,
-				minlength:3
-			},
-			phone: {
-				required:true,
-				defaultInvalid: true,
-				minlength:8
-			},
-			mail:{
-				required: true,
-				defaultInvalid: true,
-				minlength:1
-			},
-		},
-		 messages: {
-		 	name: {
-                required: "Введите ваше имя.",
+        },
+        messages: {
+            name: {
+                required: "Р’РІРµРґРёС‚Рµ РІР°С€Рµ РёРјСЏ.",
             },
             phone: {
-                required: "Введите правильный номер.",
-                minlength: "Введите правильный номер.",
+                required: "Р’РІРµРґРёС‚Рµ РїСЂР°РІРёР»СЊРЅС‹Р№ РЅРѕРјРµСЂ.",
+                minlength: "Р’РІРµРґРёС‚Рµ РїСЂР°РІРёР»СЊРЅС‹Р№ РЅРѕРјРµСЂ.",
             },
             mail: {
-                required: "Введите ваш Email.",
-                minlength: "Введите ваш Email.",
-                mail: "Неверно заполнено поле.",
+                required: "Р’РІРµРґРёС‚Рµ РІР°С€ Email.",
+                minlength: "Р’РІРµРґРёС‚Рµ РІР°С€ Email.",
+                mail: "РќРµРІРµСЂРЅРѕ Р·Р°РїРѕР»РЅРµРЅРѕ РїРѕР»Рµ.",
             },
 
-          },
-		submitHandler: function(form) {
-			$.ajax({
-				type: 'POST',
-				url: 'mailer.php',
-				data:  msg,
-				success: function(data) {
-					$.fancybox.open('#success');           
-				},
-				error:  function(xhr, str){
-					$.fancybox.open('#notsended');
-				}
-			});
-			return false;
-		}
-	});
+        },
+        submitHandler: function(form) {
+            $.ajax({
+                type: 'POST',
+                url: 'mailer.php',
+                data:  msg,
+                success: function(data) {
+                    $.fancybox.open('#success');
+                },
+                error:  function(xhr, str){
+                    $.fancybox.open('#notsended');
+                }
+            });
+            return false;
+        }
+    });
+
+    $("#form2").validate({
+
+        rules: {
+            name: {
+                required:true,
+                defaultInvalid: true,
+                minlength:3
+            },
+            phone: {
+                required:true,
+                defaultInvalid: true,
+                minlength:8
+            },
+            mail:{
+                required: true,
+                defaultInvalid: true,
+                minlength:1
+            },
+        },
+        messages: {
+            name: {
+                required: "Р’РІРµРґРёС‚Рµ РІР°С€Рµ РёРјСЏ.",
+            },
+            phone: {
+                required: "Р’РІРµРґРёС‚Рµ РїСЂР°РІРёР»СЊРЅС‹Р№ РЅРѕРјРµСЂ.",
+                minlength: "Р’РІРµРґРёС‚Рµ РїСЂР°РІРёР»СЊРЅС‹Р№ РЅРѕРјРµСЂ.",
+            },
+            mail: {
+                required: "Р’РІРµРґРёС‚Рµ РІР°С€ Email.",
+                minlength: "Р’РІРµРґРёС‚Рµ РІР°С€ Email.",
+                mail: "РќРµРІРµСЂРЅРѕ Р·Р°РїРѕР»РЅРµРЅРѕ РїРѕР»Рµ.",
+            },
+
+        },
+        submitHandler: function(form) {
+            $.ajax({
+                type: 'POST',
+                url: 'mailer.php',
+                data:  msg,
+                success: function(data) {
+                    $.fancybox.open('#success');
+                },
+                error:  function(xhr, str){
+                    $.fancybox.open('#notsended');
+                }
+            });
+            return false;
+        }
+    });
 
 
-/**
+    /**
 
-	$('#form').submit(function(e) {
+     $('#form').submit(function(e) {
 		e.preventDefault();
 		var msg  = $(this).serialize();
 
@@ -178,34 +178,34 @@ $("#form2").validate({
 		return false;
 	});
 
-**/
+     **/
 
-/*	
-
-
-	
+    /*
 
 
-	$('#form').submit(function(e) {
-		e.preventDefault();
-		var msg  = $(this).serialize();
 
-		$.ajax({
-			type: 'GET',
-			url: 'contact.php',
-			data:  msg,
-			success: function(data) {
-				$.fancybox.open('#success');           
-			},
-			error:  function(xhr, str){
-				$.fancybox.open('#notsended');
-			}
-		});
 
-		return false;
-	});
 
-	*/	
+     $('#form').submit(function(e) {
+     e.preventDefault();
+     var msg  = $(this).serialize();
+
+     $.ajax({
+     type: 'GET',
+     url: 'contact.php',
+     data:  msg,
+     success: function(data) {
+     $.fancybox.open('#success');
+     },
+     error:  function(xhr, str){
+     $.fancybox.open('#notsended');
+     }
+     });
+
+     return false;
+     });
+
+     */
 
 
 
